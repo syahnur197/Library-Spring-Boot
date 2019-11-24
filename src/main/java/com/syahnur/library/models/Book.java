c8382b7c50859b2075270b3867ade5c983c8e4e8
Syahnur <syahnurnizam197@Gmail.com>
2019-11-24 21:52:14 +0800

    Updated at and created at add 8 hours from UTC, this is annoying tbh

--- src/main/java/com/syahnur/library/models/Book.java
+++ src/main/java/com/syahnur/library/models/Book.java
@@ -1,15 +1,25 @@
 package com.syahnur.library.models;
 
-import java.io.Serializable;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.AccessLevel;
 
 @Entity
 @Getter @Setter @NoArgsConstructor 
@@ -21,15 +31,40 @@ public class Book implements Serializable {
 	private String name;
 	private String author;
 	private String isbn;
-	private String genre;
+	private String genre;    
 	
-	public Book(String name, String author, String isbn, String genre) {
+	@Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+	@CreationTimestamp
+	@Getter(AccessLevel.NONE)
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @UpdateTimestamp
+	@Getter(AccessLevel.NONE)
+    private Date updatedAt;
+
+	public Book(Integer id, String name, String author, String isbn, String genre, Date createdAt, Date updatedAt) {
 		super();
+		this.id = id;
 		this.name = name;
 		this.author = author;
 		this.isbn = isbn;
 		this.genre = genre;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
 	}
+	
+	public Date getCreatedAt() {
+		return DateUtils.addHours(this.createdAt, 8);
+	}
+	
+	public Date getUpdatedt() {
+		return DateUtils.addHours(this.updatedAt, 8);
+	}
+	
+
 	
 	
 	
